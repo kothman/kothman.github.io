737a4634dbe35287bf732e0e420b6613f673ff4b
Adrian Kothman <adrian@kothman.io>
2017-01-23 21:17:27 -0500

    Let's not use JQuery

--- main.js
+++ main.js
@@ -2,33 +2,18 @@
 	init();
 
 	function init() {
-		checkJQuery();
 		addEventListeners();	
 	}
 
-	function checkJQuery() {
-		if (!window.jQuery) {
-			console.log("jQuery is required.");
-		}
-	}
-
 	function addEventListeners() {
-		// Event listener for toggling navigation visible on mobile
-		$("#toggle").on("click", toggleNav);
-		
+		document.querySelector("#toggle").addEventListener("click", toggleNav);
 	}
 
 	function toggleNav() {
-		var nav = $(".nav-menu");
-		console.log(nav.css("display"));
-		nav.css("display") == "none" ?
-			nav.css("display", "block") :
-			nav.css("display", "");
-	}
-
-	window.showIframe = function (e) {
-		$(e.parentElement).removeClass("loading")
-		e.style.opacity = "1";
+		var nav = document.querySelector(".nav-menu");
+		nav.style.display == "" ?
+			nav.style.display = "block" :
+			nav.style.display = "" ;
 	}
 
 })();
