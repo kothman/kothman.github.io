c21538c857220b36e41ab6706c0a07654d98c3b4
Morgan Kothman <abkothman@gmail.com>
2024-10-24 10:18:16 -0400

    First commit - create the basic structure for using Org-mode' document converter.

---
+++ publish-project-setup.el
@@ -0,0 +1,116 @@
+;;; -*- lexical-binding: t -*-
+;;; Org-Mode Export - Publish
+;;; -*- lexical-binding: t -*-
+;;; Org-Mode Export - Publish
+(require 'ox-publish)
+
+;;; Helper functions for publishing the project
+(defun my-org-publish ()
+  "My function for publishing org projects as HTML."
+  (interactive)
+  (org-publish-project "org"))
+(defun my-org-publish-force ()
+  "My function for force-publishing org projects as HTML."
+  (interactive)
+  (org-publish-project "org" t))
+
+;;; Define some kbd shortcuts for publishing the project
+(keymap-global-set "C-c o P" 'my-org-publish-force)
+(keymap-global-set "C-c o p" 'my-org-publish)
+
+(defvar my-org-index-nav
+  '(about projects contact))
+(defvar my-org-nav-item-template
+  "<li id=\"nav-%s\"><a href=\"#%s\">%s</a></li>")
+(defvar my-org-nav-template
+  "<header><nav><ul>%s</ul></nav></header>"
+  "The template that gets formatted to produce the full navigation HTML.")
+(defvar my-org-nav-html
+  ""
+  "The HTML for the navigation of each page. Set by later function.")
+
+(defun my-org-build-navigation-html (navlist)
+  "Get the html for the nav section, built from a list"
+  (let ((formatted-item nil)
+	(inside-html "<li><a href=\"#title\"><h1>%t</h1><h3>%s</h3></a>"))
+    
+    (dolist
+	;;; (list-item/element, list to loop through,
+	;;;    &optional return element )
+	(list-item navlist)
+      (setq formatted-item
+	    (format my-org-nav-item-template
+		    list-item list-item list-item))
+      (setq inside-html
+	    (concat inside-html formatted-item)))
+    (setq my-org-nav-html (format my-org-nav-template inside-html)))
+  )
+
+;;; Build the html sections that should go on each page
+(my-org-build-navigation-html my-org-index-nav)
+
+;;;;;; Set org export variables
+;;;;;;
+;;; Don't export the Table of Contents
+(setq org-export-with-toc nil)
+;;; Don't export drawers
+(setq org-export-with-drawers nil)
+;;; Don't export section numbers
+(setq org-export-with-section-numbers nil)
+;;; Mark broken links
+(setq org-export-with-broken-links 'mark)
+;;; Remove the standard title
+(setq org-export-with-title nil)
+
+;;;;;;
+;;;;;; Set HTML-specific export variables
+;;;;;;
+;;; Replace the title custom preamble
+(setq org-html-preamble t)
+;;; Define the preamble format for each language here
+(setq org-html-preamble-format
+      `(("en" ,my-org-nav-html)))
+;;; Customize Postamble
+(setq org-html-postamble nil)
+;;; Include stylesheet in the head
+(setq org-html-head "<link rel=\"stylesheet\" href=\"styles.css\">")
+(setq org-html-head-extra nil)
+;;; No JavaScript for now
+(setq org-html-head-include-scripts nil)
+;;; Set our own styles in org-html-head per spec
+(setq org-html-head-include-default-style nil)
+;;; Not using InfoJS
+(setq org-html-use-infojs nil)
+;;; Scripts are also set in the org-html-head, if needed
+(setq org-html-scripts nil)
+;;; Using new HTML5 elements
+(setq org-html-html5-fancy t)
+;;; Using html5 doctype
+(setq org-html-doctype "html5")
+;;; Defaults to div, but I like it to be obvious
+(setq org-html-container-element "div")
+;;; Defaults to content, but I like it to be obvious
+(setq org-html-content-class "content")
+
+;;;;;;
+;;;;;; Setup org projects for each export type
+;;;;;;
+(setq org-publish-project-alist
+      '(
+	;;; Org Notes exports .org files to HTML5
+	("org-notes"
+	 :base-directory "~/code/static-streamline/src/"
+	 :base-extension "org"
+	 :publishing-directory "~/code/static-streamline/public/"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html
+	 :headline-levels 4
+	 :auto-preamble t)
+	("org-static"
+	 :base-directory "~/code/static-streamline/src/"
+	 :base-extension "css\\|js\\|png\\|jpg\\|jpeg\\|gif"
+	 :publishing-directory "~/code/static-streamline/public/"
+	 :recursive t
+	 :publishing-function org-publish-attachment)
+	("org" :components ("org-notes" "org-static"))
+	))
